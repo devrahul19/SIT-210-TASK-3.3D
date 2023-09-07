e940f50997f37ac6505adc1174f291b50d840391
Rahul Dhiman <122608786+devrahul19@users.noreply.github.com>
2023-09-07 16:23:24 +0530

    ModifiedCode.ino

--- ModifiedCode.ino
+++ ModifiedCode.ino
@@ -1,1 +1,35 @@
+const int ledPin = 12; 
+bool isPatSignal = false;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Particle.subscribe("buddyPhotonSignal", signalHandler);
+}
+
+void loop() {
+
+}
+
+void signalHandler(const char *event, const char *data) {
+  if (strcmp(data, "pat") == 0) {
+    isPatSignal = true;
+  } else if (strcmp(data, "wave") == 0) {
+    isPatSignal = false;
+  }
+}
+
+void flashLED() {
+  if (isPatSignal) {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  } else {
+ 
+    digitalWrite(ledPin, HIGH);
+    delay(500); 
+    digitalWrite(ledPin, LOW);
+    delay(500);
+  }
+}
 
