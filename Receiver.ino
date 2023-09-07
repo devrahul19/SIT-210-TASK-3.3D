89d3fb1fe853160f8ecbd92c7de263df7af7c297
Rahul Dhiman <122608786+devrahul19@users.noreply.github.com>
2023-09-07 16:10:09 +0530

    Receiver.ino

--- Receiver.ino
+++ Receiver.ino
@@ -1,10 +1,8 @@
 #include <ArduinoMqttClient.h>
-#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) 
 #include <WiFiNINA.h>
 #elif defined(ARDUINO_SAMD_MKR1000)
 #include <WiFi101.h>
-#elif defined(ARDUINO_ARCH_ESP8266)
-#elif defined(ARDUINO_ARCH_ESP32)
 #include <WiFi.h>
 #endif
 
